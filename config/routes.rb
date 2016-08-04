ca6190b52e39015586e69ef792570dc884c385de
bilel-kedidi <bilel.kedidi@gmail.com>
2016-08-04 22:46:23 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-  match 'bbb', :controller => :bbb, :action => :start
+  match 'bbb', :controller => :bbb, :action => :start, via: [:get, :post]
   get 'bbb/:action', :controller => :bbb
 end
